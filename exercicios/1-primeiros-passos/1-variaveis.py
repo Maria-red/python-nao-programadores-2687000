3b6bc2b255c2022ef7558cf2bc17e98069008300
Maria-red <maddie.piecew@gmail.com>
2024-08-20 17:44:39 +0000

    resolução de exercicio

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -2,10 +2,16 @@
 # - Iniciar sempre com letra minúscula;
 # - O nome pode ser composto por letras, algarismos e underlines;
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
-
+nome = 'paloma'
+profissão = "vendedora"
 # Declare variáveis do tipo string
-
+objetivo = "'
+aprender python
+para mudar
+de profissão.'"
 # Declare variáveis de cada tipo numérico
-
-
+número_inteiro = 6
+salário = 1212.00
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = false
